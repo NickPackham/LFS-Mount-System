fd7db97afd1a329609d0aaa329763fc345dc3fa4
Nick Packham <46549847+NickPackham@users.noreply.github.com>
2020-08-18 02:48:51 -0700

    Config

--- lua/entities/eps_mounts_npc/shared.lua
+++
@@ -1,8 +0,0 @@
-ENT.Base = "base_anim"
-ENT.Type = "ai"
-ENT.Base = "base_gmodentity"
-
-ENT.PrintName = "Mounts NPC"
-ENT.AuthorName = ""
-ENT.Category = "Mounts NPC"
-ENT.Spawnable = true
--- lua/eps_mounts/server/eps_mounts_main_sv.lua
+++
@@ -1,3 +0,0 @@
-hook.Add("EPS_PlayerReadyForNetworking", "EPS_Mounts_ReadyForNetworking", function(ply)
-	ply:EPS_UpdatePurchasedMounts()
-end)
